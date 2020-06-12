227d24cc0146cd7b4b63c9c303d02edb96c94e99
Coralie B <coralie@mbpdecoralie2.home>
2020-06-12 19:15:28 +0200

    add css card film - icon search

--- src/components/Film.js
+++ src/components/Film.js
@@ -9,6 +9,9 @@ const Film = ({ changeView, film}) => {
 
   return (
     <Card onClick={(e, film) => handleClickFilm(e, film)}>
+      <div className="look">
+        <a href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+      </div>
       <Card.Img className="img-list" variant="top" src="https://picsum.photos/200/300" />
       <Card.Body>
         <Card.Title>{film.title}</Card.Title>
